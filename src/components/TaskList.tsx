dd308bbce6ff5ec8c1190a256aed39fdde50a26b
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-03-14 14:00:46 -0300

    Feat. Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return      //Verificação para caso o o título seja vazio
+
+    const newTask = {             // Criando um nova Tasks
+      id: Math.random(),          // Gerando um ID aleatório
+      title: newTaskTitle,        // Pegando o valor do input e atribuindo a Props title
+      isComplete: false           // Iniciando com o valor False
+    }
+
+    setTasks(oldState => [...oldState, newTask])  // Atualizando o antigo State das Tasks, add a nova Task
+    setNewTaskTitle('')                           // Deixa o input vazio
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(tasks => tasks.id == id ? {    // Percorre as Tasks que tem um ID igual do input
+      ...tasks,
+      isComplete: !tasks.isComplete   // Inverte o valor da isComplete             
+    } : tasks)                        // Retorna as Tasks já cadastradas caso o ID não foi encontrado
+
+    setTasks(newTasks)                // Cria uma nova instância com as atualizações
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(tasks => tasks.id !== id)  // Cria um novo Array com as Tasks, exceto a Tasks com ID do input
+
+    setTasks(filteredTasks)           // Cria uma nova instância com as atualizações
   }
 
   return (
